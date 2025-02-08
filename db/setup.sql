f1757a5111257ec4be3cb31ea5110793b879ae66
Oli <oli@olillin.com>
2025-02-08 17:03:31 +0100

    Update database tables

--- db/setup.sql
+++ db/setup.sql
@@ -1,45 +1,45 @@
 USE PritStreck;
 
 CREATE TABLE Groups (
-    Id INT UNIQUE NOT NULL,
-    DisplayName VARCHAR(32) NOT NULL,
-    IconUrl VARCHAR(255) NOT NULL,
-    PRIMARY KEY (Id)
-);
-
-CREATE TABLE Users (
     GammaId VARCHAR(255) UNIQUE NOT NULL,
     PRIMARY KEY (GammaId)
 );
 
-CREATE TABLE UserGroups (
-    UserId VARCHAR(255) UNIQUE NOT NULL,
-    GroupId INT NOT NULL,
-    Administrator BOOLEAN NOT NULL,
-    FOREIGN KEY (UserId) REFERENCES Users(GammaId),
-    FOREIGN KEY (GroupId) REFERENCES Groups(Id)
+CREATE TABLE Users (
+    GammaId VARCHAR(255) UNIQUE NOT NULL,
+    Balance INT NOT NULL,
+    PRIMARY KEY (GammaId)
 );
 
 CREATE TABLE Items (
     Id INT UNIQUE NOT NULL,
+    GroupId VARCHAR(255) NOT NULL,
     DisplayName VARCHAR(32) NOT NULL,
-    IconUrl VARCHAR(255) NOT NULL,
+    Price INT NOT NULL,
+    IconUrl VARCHAR(255),
     TimesPurchased INT NOT NULL,
-    PRIMARY KEY (Id)
-)
+    Visible BOOLEAN NOT NULL,
+    PRIMARY KEY (Id),
+    FOREIGN KEY (GroupId) REFERENCES Groups(GammaId)
+);
 
 CREATE TABLE Purchases (
     Id INT UNIQUE NOT NULL,
+    GroupId INT NOT NULL,
     PurchasedBy VARCHAR(255) UNIQUE NOT NULL,
     PurchasedFor VARCHAR(255) UNIQUE NOT NULL,
-    GroupId INT NOT NULL,
     PurchasedDate DATETIME NOT NULL,
     PRIMARY KEY (Id),
     FOREIGN KEY (PurchasedBy) REFERENCES Users(GammaId),
     FOREIGN KEY (PurchasedFor) REFERENCES Users(GammaId),
-    FOREIGN KEY (GroupId) REFERENCES Groups(Id)
+    FOREIGN KEY (GroupId) REFERENCES Groups(GammaId)
 );
 
 CREATE TABLE PurchasedItems (
-    I
-)
+    PurchaseId INT NOT NULL,
+    ItemId INT NOT NULL,
+    Quantity INT NOT NULL,
+    CurrentPrice INT NOT NULL,
+    FOREIGN KEY (PurchaseId) REFERENCES Purchases(Id),
+    FOREIGN KEY (ItemId) REFERENCES Items(Id)
+);
